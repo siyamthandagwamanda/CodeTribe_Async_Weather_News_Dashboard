194336900e491051f60e8ee87fefd4bb148669d6
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-09-14 10:56:04 +0200

    feat: Define Typescript structural contract for parsing the optimized Open-Meteo current_weather payload

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -3,3 +3,14 @@ import https from 'https'
 type Callback<T> = (error: Error | null, data?: T) => void;
 
 const WEATHER_URL = 'https://api.open-meteo.com/v1/forecast?latitude=-29.6168&longitude=30.3928&current_weather=true';
+
+interface WeatherResponse{
+    current_weather:
+    {
+        temperature: number;
+        windspeed: number;
+        weathercode: number;
+        time: string;
+    }
+}
+
