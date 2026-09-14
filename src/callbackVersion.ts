f0fa97b5f0fd42241f7820bbe52c419b45a9891d
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-09-14 10:41:36 +0200

    feat: update weather_url constant with precise latitude nand longitude mapping for Pietermaritzburg.

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -1,3 +1,5 @@
 import https from 'https'
 
 type Callback<T> = (error: Error | null, data?: T) => void;
+
+const WEATHER_URL = 'https://api.open-meteo.com/v1/forecast?latitude=-29.6168&longitude=30.3928&current_weather=true';
