091367f5086569f9d46057edda8b301e8fc1eb5f
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-09-14 11:23:04 +0200

    refactor: make callback type generic in callbackVersion.ts

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -1,6 +1,6 @@
 import https from 'https'
 
-type Callback<T> = (error: Error | null, data?: T) => void;
+type Callback<ResponseData> = (error: Error | null, data?: T) => void;
 
 const WEATHER_URL = 'https://api.open-meteo.com/v1/forecast?latitude=-29.6168&longitude=30.3928&current_weather=true';
 
