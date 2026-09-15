4297d5ae86626483c78d32e904a33d5f46a5f163
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-09-15 10:27:32 +0200

    feat: add weather and news API structures and endpoints

--- src/promiseVersion.ts
+++ src/promiseVersion.ts
@@ -1,1 +1,23 @@
 import http from 'https';
+
+const WEATHER_URL =  'https://api.open-meteo.com/v1/forecast?latitude=-29.6168&longitude=30.3928&current_weather=true';
+
+const NEWS_URL = 'https://dummyjson.com/posts?limit=5';
+
+interface WeatherResponse{
+    currrent_weather: {
+        temperature: number;
+        windspeed: number;
+        weathercode: number;
+        time: string;
+    }
+}
+
+interface Post{
+    id: number;
+    title: string;
+}
+
+interface NewsResponse{
+    posts: Post[];
+}
